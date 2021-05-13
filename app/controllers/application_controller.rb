1a2d74c8c1102c37a29524e0887aeed7c44e27ae
adajacobs <adam.jacobs4@gmail.com>
2021-05-13 15:54:49 -0400

    added order items and cart activity

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+
+  before_action :current_cart
+  helper_method :current_cart
+
+  def current_cart
+    # if the customer has just landed on the site, give them a brand new cart
+    # if theyu already have one, keep it
+    # check session[:cart_id]
+
+    if session[:cart_id].present?
+      #if they already have a cart
+      @current_cart = Cart.find(session[:cart_id])
+    else
+      #if they don't
+      @current_cart = Cart.create
+      session[:cart_id] = @current_cart.id
+    end
+  end
+
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,6 +6,14 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
+
+    # if the product is already in the cart
+    @order_item = @current_cart.order_items.find_by(product: @product)
+
+    if @order_item.nil?
+      # if it doesnt exist in the cart
+      @order_item = @current_cart.order_items.new(product: @product, quantity: 1)
+    end
   end
 
 end
